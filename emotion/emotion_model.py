b7b86951a57c4b54dcac2eace0afd9b2206096f8
Pritam Rao <pritam.rao@spit.ac.in>
2020-11-04 22:31:30 +0530

    Emotion Detected successfully

--- emotion/emotion_model.py
+++ emotion/emotion_model.py
@@ -1,34 +1,70 @@
-# from tensorflow.keras.models import model_from_json
-# import numpy as np
-# import tensorflow as tf
-# import cv2
+from tensorflow.keras.models import model_from_json,load_model
+import numpy as np
+import tensorflow as tf
+import cv2
 
-# class FacialExpressionModel(object):
-#     EMOTION_LIST = ["Angry" ,"Disgust" ,"Fear" ,"Happy","Sad","Surprise","Neutral"]
+class FacialExpressionModel(object):
+    EMOTION_LIST = ["Happy" ,"Neutral" ,"Sad"]
+    emotion_output = None
+    IMG_SIZE = 256
 
-#     def __init__(self,model_json_file,model_weights_file,img):
-#         with open(model_json_file,"r") as json_file:
-#             loaded_model_in_json = json_file.read() 
-#             self.loaded_model = model_from_json(loaded_model_in_json)
+    def __init__(self,model_json_file,model_weights_file,img_file):
 
-#         self.image = cv2.imread(img)
-#         self.image = cv2.resize(self.image,(48,48))  
-#         self.image = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
-#         #cv2.imshow("image",self.image)
-#         #print(self.image.shape)
-#         self.loaded_model.load_weights(model_weights_file)
+        # Loading Model
+        with open(model_json_file,"r") as json_file:
+            loaded_model_in_json = json_file.read() 
+            self.model = model_from_json(loaded_model_in_json)
+            self.model.load_weights(model_weights_file)
 
-#         #print("\n\n\n\n")
-#         #print(self.loaded_model.summary())
+        # Loading Image
+        self.image = cv2.imread(img_file)
+
+        # Loading FaceDetector
+        self.face_cascade = cv2.CascadeClassifier("emotion/haarcascade_frontalface_default.xml")
+          
+        
+    
+    def preprocessImg(self):
+        self.image = cv2.cvtColor(self.image, cv2.COLOR_BGR2RGB)
+        roi_img = None
+
+        ## Detecting Face 
+        try:
+            faces = self.face_cascade.detectMultiScale(self.image,1.3,5)
+            if len(faces)==0:
+                print(f"No Face detected for given image ,Try again with new img :(")
+                return False,None
+            elif len(faces)>1:
+                print(f"Multiple Faces detected for given image ,Try again with new img :(")
+                return False,None
+            else:
+                for (x,y,w,h) in faces:
+                    roi_img = self.image[y:y+h, x:x+w]
+
+                roi_img = cv2.resize(roi_img,(self.IMG_SIZE,self.IMG_SIZE))
+                roi_img = roi_img/255.0
+
+        except Exception as _:
+            print("An Error Ocurred, Please Try Again :(")
+            return False,None
+        else:
+            return True,roi_img
+
+        
+    def predictEmotion(self,roi_img):
+        try:
+            labels =  ["happy","neutral","sad"]
+            prediction = self.model.predict(roi_img.reshape(1,self.IMG_SIZE,self.IMG_SIZE,3))
+            
+            self.emotion_output = labels[np.argmax(prediction)]
+
+        except Exception as _:
+            print("An Error Ocurred, Please Try Again :(")
+            return False
+        else:
+            return True
 
 
-#     def predict_emotion(self):
-#         ## Image reshaping wala part dekh lena
-
-#         input_img = self.image/255
-#         input_img = input_img.reshape((1,48,48,1))
-#         self.preds = self.loaded_model.predict(input_img)
-
-#         return self.EMOTION_LIST[np.argmax(self.preds)]
-
+    def getEmotions(self):
+        return self.emotion_output
 
--- emotion/views.py
+++ emotion/views.py
@@ -1,18 +1,35 @@
 from django.shortcuts import render,HttpResponse
 from . import emotion_model 
-from PIL import Image
+from Image.models import Image
+from pathlib import Path
+import os
+BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # Create your views here.
 
 def emotion(request):
-    JSON_PATH = "/home/pritamrao/Desktop/TE-SEM-5-6/projects/music/emotion/model.json"
-    WEIGHTS = "/home/pritamrao/Desktop/TE-SEM-5-6/projects/music/emotion/model_weights.h5"
-    IMG = "/home/pritamrao/Desktop/TE-SEM-5-6/projects/music/static/images/surprise.webp"
+    JSON_PATH = str(BASE_DIR)+"/emotion/model_config.json"
+    WEIGHTS_PATH = str(BASE_DIR)+"/emotion/model_wts.h5"
+    
+    ## Getting ImgPath
+    image = Image.objects.get(user=request.user)
+    image_url = image.Image_url
+    IMG_PATH = str(BASE_DIR)+"/media/"+str(image_url)
 
-    fer = emotion_model.FacialExpressionModel(JSON_PATH,WEIGHTS,IMG)
-    emotion = fer.predict_emotion()
 
-    context = {'emotion':emotion}
+    fer = emotion_model.FacialExpressionModel(JSON_PATH,WEIGHTS_PATH,IMG_PATH)
+    return_val,processed_img = fer.preprocessImg()
 
+    emotion = ""
+    if return_val:
+        success = fer.predictEmotion(processed_img)
+        if success:
+            emotion += fer.getEmotions()
+            print(f'\n\n\nEMOTION IS {emotion}\n\n\n')
 
+            ## After succesful removal of emotion delete the image
+            image.delete()
+    else:
+        print("PreProcess me error")
+
+    context = {'emotion':emotion,'image':image_url}
     return render(request,'emotion/index.html',context)
-    #return HttpResponse("hello")
