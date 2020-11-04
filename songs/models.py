66865dc9898679937ff91a66993b5f3e7da8ecc2
mahipal purohit <mahipalkeizer@gmail.com>
2020-11-04 11:18:17 +0530

    admin created and songs added

--- songs/models.py
+++ songs/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Song(models.Model):
 	user = models.ForeignKey(User,default=None,on_delete=models.CASCADE)
 	song_name= models.CharField(max_length=100)
-	song_url=models.CharField(max_length=100)
+	song_url=models.CharField(max_length=400)
 	mood_options=[('sad', 'sad'),('happy', 'happy'),('normal','normal')]
 	emotion = models.CharField(max_length=50,choices=mood_options,default='normal')
 	
