7b3074d74afaf36cd660fc54be092d7ff32b4f32
Pritam Rao <pritam.rao@spit.ac.in>
2020-11-08 18:35:47 +0530

    Playlist loading succesfully after emotion

--- emotion/views.py
+++ emotion/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render,redirect,HttpResponse
 from . import emotion_model 
 from Image.models import Image
+from playlists.models import Playlist,Playlist_songs
+from songs.models import Song
+from django.contrib.auth.models import User
 from pathlib import Path
+import random
 import os
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # Create your views here.
@@ -22,13 +26,13 @@ def emotion(request):
     image.delete()
     return_val,processed_img = fer.preprocessImg()
     
-    if return_val==0:
+    if return_val=="noFace":
         error="No Face detected for given image ,Try again with new img :("
         return redirect("error-page",error)
-    elif return_val==2:
+    elif return_val=="mulFace":
         error="Multiple Face detected for given image ,Try again with new img :("
         return redirect("error-page",error)
-    elif return_val==1:
+    elif return_val=="exception":
         error="An Unkwon Error Ocurred, Please Try Again :("
         return redirect("error-page",error)
 
@@ -42,5 +46,29 @@ def emotion(request):
     else:
         print("PreProcess me error")
     
-    context = {'emotion':emotion,'image':image_url}
-    return render(request,'emotion/index.html',context)
+    #context = {'emotion':emotion,'image':image_url}
+    #####
+    user_songs=Song.objects.filter(user=request.user).filter(emotion=emotion)
+    admin =User.objects.filter(username='admin')[0]
+    admin_songs=Song.objects.filter(user=admin).filter(emotion=emotion)
+    playlist=Playlist(user=admin,playlist_name=emotion,
+                number_of_songs=-1)
+    playlist.save()
+    for song in user_songs:
+        relation=Playlist_songs(playlist=playlist,song=song)
+        relation.save()
+    sz=admin_songs.count()
+    
+    sampling = set(random.choices(list(range(0,sz)), k=min(10,sz)))
+    i=0 
+    for song in admin_songs:
+        if i in sampling:
+            relation=Playlist_songs(playlist=playlist,song=song)
+            relation.save()
+        i+=1
+    return redirect('displayPlaylist',playlist.id)
+    ####
+    
+    
+    
+    
